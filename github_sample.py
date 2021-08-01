4b7f1e26f85b2f98643e580eced192343e9f017f
Denzi12 <chackokavanattu12@gmail.com>
2021-08-01 13:47:25 +0530

    Update github_sample.py

--- github_sample.py
+++ github_sample.py
@@ -15,5 +15,17 @@ def show_user(request, username):
         # BAD -- Manually quoting placeholder (%s)
         cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
         user = cursor.fetchone()
+        
+        cursor.execute("SELECT admin FROM users WHERE username = '" + username + '");
+        user = cursor.fetchone()
+
+        cursor.execute("SELECT admin FROM users WHERE username = '%s' % username);
+        user = cursor.fetchone()
+
+        cursor.execute("SELECT admin FROM users WHERE username = '{}'".format(username));
+        user = cursor.fetchone()
+
+        cursor.execute(f"SELECT admin FROM users WHERE username = '{username}'");
+        user = cursor.fetchone()
 
 urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
